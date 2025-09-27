76bc32b073a314db222633d2b40bdce6556177f6
Adelaide Erin Tovar <13893006+adelaidetovar@users.noreply.github.com>
2025-09-27 09:48:39 -0400

    update read stats for modified pipeline

--- bc_count/read_stats.py
+++ bc_count/read_stats.py
@@ -33,51 +33,53 @@ def main():
                 deduplicated_barcodes = os.path.join(in_umibc, f"{libname}.starumi") if in_umibc else None
                 clustered_barcodes = os.path.join(in_clust, f"{libname}.bc_cluster.txt")
 
-                # input read pairs, adapter read pairs from umitools
-                pass_umitools = 0
-                pass_umitools_percentage = "N/A"
-                with open(os.path.join(in_umi, umitools_log), 'r') as f:
-                    umitools_in = f.read()
-                    try:
-                        input_reads = int(re.search(r"Input Reads:\s+([\d]+)", umitools_in).group(1))
-                        pass_umitools = int(re.search(r"Reads output:\s+(\d+)", umitools_in).group(1))
-                        pass_umitools_percentage = "{:.2f}%".format((pass_umitools / input_reads) * 100)
-                    except AttributeError:
-                        print(f"Error: unable to parse {umitools_log}!")
-                        continue
-
-                # reads with barcode from cutadapt
+                # input reads, reads with barcode from cutadapt
+                pass_cutadapt = 0
+                pass_cutadapt_percentage = "NA"
                 with open(os.path.join(in_clip, cutadapt_log), 'r') as f:
                     cutadapt_in = f.read()
                     try:
+                        input_reads = int(re.search(r"Total reads processed:\s+([\d,]+)", cutadapt_in).group(1).replace(',', ''))
                         pass_cutadapt = int(re.search(r"Reads written \(passing filters\):\s+([\d,]+)", cutadapt_in).group(1).replace(',', ''))
                         pass_cutadapt_percentage = "{:.2f}%".format((pass_cutadapt / input_reads) * 100)
                     except AttributeError:
                         print(f"Error: unable to parse {cutadapt_log}!")
                         continue
 
-                    # deduplicated barcodes
+                # reads with umi from umitools
+                pass_umitools = 0
+                pass_umitools_percentage = "N/A"
+                with open(os.path.join(in_umi, umitools_log), 'r') as f:
+                    umitools_in = f.read()
+                    try:
+                        pass_umitools = int(re.search(r"Reads output:\s+(\d+)", umitools_in).group(1))
+                        pass_umitools_percentage = "{:.2f}%".format((pass_umitools / input_reads) * 100)
+                    except AttributeError:
+                        print(f"Error: unable to parse {umitools_log}!")
+                        continue
+
+                # deduplicated barcodes
                 deduplicated_barcode_count = 0
                 if deduplicated_barcodes and os.path.exists(deduplicated_barcodes):
                     with open(deduplicated_barcodes, 'r') as f:
                         deduplicated_barcode_count = sum(1 for _ in f) - 1
 
-                    # clustered barcodes
+                # clustered barcodes
                 clustered_barcode_count = 0
                 if os.path.exists(clustered_barcodes):
                     with open(clustered_barcodes, 'r') as f:
                         clustered_barcode_count = sum(1 for _ in f) - 1
 
-                    # add to summary table
-                summary_data.append([libname, input_reads, pass_umitools, pass_umitools_percentage, pass_cutadapt, pass_cutadapt_percentage,
+                # add to summary table
+                summary_data.append([libname, input_reads, pass_cutadapt, pass_cutadapt_percentage, pass_umitools, pass_umitools_percentage, 
                                     deduplicated_barcode_count, clustered_barcode_count])
 
         # save to a file
-        columns = ["Sample", "Input_Reads", "Pass_Umitools_Reads", "Pass_Umitools_Percentage", 
-                    "Pass_Cutadapt_Reads", "Pass_Cutadapt_Percentage", "Deduplicated_Amplicons", "Clustered_Barcodes"]
+        columns = ["Sample", "Input_Reads", "Pass_Cutadapt_Reads", "Pass_Cutadapt_Percentage",
+                   "Pass_Umitools_Reads", "Pass_Umitools_Percentage","Deduplicated_Amplicons", "Clustered_Barcodes"]
         summary_df = pd.DataFrame(summary_data, columns=columns)
         summary_df.sort_values(by = "Sample", inplace=True)
-        comma_cols = summary_df.columns.difference(["Sample", "Pass_Umitools_Percentage", "Pass_Cutadapt_Percentage"])
+        comma_cols = summary_df.columns.difference(["Sample", "Pass_Cutadapt_Percentage", "Pass_Umitools_Percentage", ])
         summary_df[comma_cols] = summary_df[comma_cols].apply(lambda col: col.map(lambda x: f"{x:,}" if pd.notnull(x) else x))
         summary_df.to_csv(out_tab, sep='\t', index=False)
 
